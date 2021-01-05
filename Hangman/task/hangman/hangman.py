8f6e1edcc7dc87b2f182dc257177ae55c2319c81
Sergey Goppikov <grey2k@gmail.com>
2021-01-06 00:20:58 +0300

    Hangman finished

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 # Write your code here
 GAME = 'hangman'
@@ -6,6 +7,14 @@ LIVES = 8
 DICTIONARY = ['python', 'java', 'kotlin', 'javascript']
 
 print(' '.join([ch.upper() for ch in list(GAME)]))
+
+while True:
+    command = input('Type "play" to play the game, "exit" to quit: ')
+    if command == 'play':
+        break
+
+    if command == 'exit':
+        exit(0)
 
 
 def generate_word():
@@ -27,29 +36,40 @@ def get_hint(secret: str, hints: set) -> str:
 
 word = generate_word()
 lives = LIVES
-letters = set()
+
+found = set()
+guessed = set()
 word_set = set(word)
 
 while True:
     if lives <= 0:
         break
 
-    print("\n{hint}".format(hint=get_hint(word, letters)))
+    print("\n{hint}".format(hint=get_hint(word, found)))
     letter = input('Input a letter: ')
+
+    if len(letter) != 1:
+        print('You should input a single letter')
+        continue
+
+    if letter not in string.ascii_lowercase:
+        print('Please enter a lowercase English letter')
+        continue
+
+    if letter in guessed:
+        print("You've already guessed this letter")
+        continue
+
+    guessed.add(letter)
 
     if letter not in word_set:
         print("That letter doesn't appear in the word")
         lives -= 1
         continue
 
-    if letter in letters:
-        print('No improvements')
-        lives -= 1
-        continue
+    found.add(letter)
 
-    letters.add(letter)
-
-    if letters == word_set:
+    if found == word_set:
         print(word)
         print("You guessed the word!")
         break
